6c3f219c052bcba79234290a476234d27e3373dd
Rickey <weems505@gmail.com>
2020-12-16 17:22:44 -0500

    update model

---
+++ BookRentalSpringBoot/src/main/java/com/example/demo/models/BookModel.java
@@ -0,0 +1,59 @@
+package com.example.demo.models;
+
+
+public class BookModel {
+
+    private long id;
+    private String title;
+    private long publishYear;
+    private String author;
+    private String publisher;
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+//    public BookModel(String title, long publishYear, String author, int numOfPages) {
+//        this.title = title;
+//        this.publishYear = publishYear;
+//        this.author = author;
+//    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPublishYear(long publishYear) {
+        this.publishYear = publishYear;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public long getPublishYear() {
+        return publishYear;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+
+    @Override
+    public String toString() {
+        return "BookModel{" +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publishYear=" + publishYear +
+                ", publisher='" + publisher + '\'' +
+                '}';
+    }
+}
