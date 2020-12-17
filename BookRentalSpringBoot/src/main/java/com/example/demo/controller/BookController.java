a810cc77f81a9e4523ea09816d16e8395cf131cc
Rickey <weems505@gmail.com>
2020-12-17 09:54:02 -0500

    removed hard coded value

--- BookRentalSpringBoot/src/main/java/com/example/demo/controller/BookController.java
+++ BookRentalSpringBoot/src/main/java/com/example/demo/controller/BookController.java
@@ -6,6 +6,7 @@ import org.json.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,19 +21,27 @@ public class BookController {
         return new RestTemplate();
     }
 
-    @GetMapping("/getbook")
-    public String getBook() throws JSONException {
+    @GetMapping("/getbook/{bookTitle}")
+    public String getBook(@PathVariable String bookTitle) throws JSONException {
+        if(bookTitle.contains(" ")){
+            bookTitle = bookTitle.replaceAll("\\s", "_" );
+        }
+        System.out.println(bookTitle);
 
         BookModel book = new BookModel();
-        String bookList = restTemplate.getForObject("https://openlibrary.org/search.json?q=clean_code", String.class);
+        String url = "https://openlibrary.org/search.json?q=" + bookTitle;
+        System.out.println(url);
+        String bookList = restTemplate.getForObject(url, String.class);
         JSONObject jsonObject = new JSONObject(bookList).getJSONArray("docs").getJSONObject(0);
 
         book.setTitle(jsonObject.getString("title"));
         book.setPublishYear(Long.parseLong(jsonObject.getString("first_publish_year")));
+
         if(jsonObject.has("author_name"))
             book.setAuthor(jsonObject.getJSONArray("author_name").get(0).toString());
         else
             book.setAuthor("no author found");
+
         book.setPublisher(jsonObject.getJSONArray("publisher").get(0).toString());
 
         return book.toString();
