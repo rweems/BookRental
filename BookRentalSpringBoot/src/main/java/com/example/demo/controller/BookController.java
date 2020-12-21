35122b19fb6fddd91e433895d50ed37041703ddd
Rickey <weems505@gmail.com>
2020-12-20 19:13:44 -0500

    updated controller

--- BookRentalSpringBoot/src/main/java/com/example/demo/controller/BookController.java
+++ BookRentalSpringBoot/src/main/java/com/example/demo/controller/BookController.java
@@ -6,6 +6,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class BookController {
 
 
